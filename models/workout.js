f311cb38aae9e5c0ead1cddebd3c7446436962ca
adeha047 <dehaven7777@gmail.com>
2021-04-12 16:21:09 -0400

    added new schema and the index.js

--- models/workout.js
+++ models/workout.js
@@ -2,23 +2,49 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({ 
-
+const WorkoutSchema = new Schema({
     day: {
         type: Date,
-        default:() => new Date()
-      },
-      notes: [
+        default: () => new Date()
+    },
+    exercises: [
         {
-          type: Schema.Types.ObjectId,
-          ref: "Note"
+            type: {
+                type: String,
+                trim: true,
+                required: "Please add a type of exercise."
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Please add the name of the exercise."
+            },
+
+            weight: {
+                type: Number
+            },
+
+            sets: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            duration: {
+                type: Number,
+                required: "Please add the duration of the exercise."
+            },
+            distance: {
+                type: Number
+            }
         }
-      ]
+    ]
+});
 
-
-
-})
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
+
+
+// I want to be able to log multiple exercises in a workout on a given day. I should also be able to track the name, type, weight, sets, reps, and duration of exercise. If the exercise is a cardio exercise, I should be able to track my distance traveled.
