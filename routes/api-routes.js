3f63c8d687d4d8d92162ad85301ae88497e67a23
adeha047 <dehaven7777@gmail.com>
2021-04-13 18:37:51 -0400

    added new duration route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,15 +1,15 @@
 const router = require("express").Router();
 const db = require("../models");
 
-router.get("/api/workouts", (req, res) => {
-  db.Workout.find({}).then(dbWorkout => {
-    res.json(dbWorkout);
+// router.get("/api/workouts", (req, res) => {
+//   db.Workout.find({}).then(dbWorkout => {
+//     res.json(dbWorkout);
 
-  })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
+//   })
+//     .catch(err => {
+//       res.status(400).json(err);
+//     });
+// });
 
 router.post("/api/workouts", ({ body }, res) => {
   db.Workout.create(body)
@@ -21,54 +21,33 @@ router.post("/api/workouts", ({ body }, res) => {
     });
 });
 
-router.post("/api/workouts", (req, res) => {
-  db.Workout.find({}).then(dbWorkout => {
-    db.Workout.aggregate(
-      [
-        {
-          $addFields: {
-            totalduration: { $sum: "$workouts" }
-
-          }
-        }, 
-        {
-          $addFields: { totalDuration:
-            { $add: [ "$totalWorkout"]}}
+router.get("/api/workouts", (req, res) => {
+  db.Workout.aggregate(
+    [
+      {
+        $addFields: {
+          totalduration: { $sum: "$duration" }
         }
-      ]).then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  })
-})
+      }
+    ]).then(dbWorkout => {
+      console.log(JSON.stringify(dbWorkout, null, 2));
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
 
-  //   router.put("/api/workouts/:id", (req, res) => {
+router.put("/api/workouts/:id", (req, res) => {
+  db.Workout.findOneAndUpdate({ _id: req.params.id }, { $push: { exercises: req.body } })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
-  //     db.Workout.findOneAndUpdate(
-  //         { _id: req.params.id },
-  //         {
-  //             $inc: { totalDuration: req.body.duration },
-  //             $push: { exercises: req.body }
-  //         },
-  //         { new: true }).then(dbWorkout => {
-  //             res.json(dbWorkout);
-  //         }).catch(err => {
-  //             res.json(err);
-  //         });
-  // });
-
-  router.put("api/workouts/:id", (req, res) => {
-    db.Workout.findOneAndUpdate({ _id: req.params.id }, { $push: { exercises: req.body } })
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-
-  module.exports = router;
+module.exports = router;
 
   // I want to be able to log multiple exercises in a workout on a given day. I should also be able to track the name, type, weight, sets, reps, and duration of exercise. If the exercise is a cardio exercise, I should be able to track my distance traveled.
